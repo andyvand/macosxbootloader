740db1a0ed39ab8f5c8539aa4d2ed122ffbb497e
Andy Vandijck <andyvand@gmail.com>
2014-12-02 20:56:58 +0100

    Added universal EFI binary build script, updated TODO.txt, changed efilipo build settings to no code signing by default, built efilipo and installer 32 + 64 bit and updated README file

---
+++ build_universal.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+
+### Universal EFI binary build script for Xcode and GNU tools ###
+make clean
+
+if [ "${CLOVERTOOLS}" = "1" ]; then
+echo "Building GNU x64 boot.efi"
+make CLOVERTOOLS=1
+else
+echo "Building Apple x64 boot.efi"
+make
+fi
+
+cp src/boot/boot.efi boot.64.efi
+make clean
+
+if [ "${CLOVERTOOLS}" = "1" ]; then
+echo "Building GNU x86 boot.efi"
+make CLOVERTOOLS=1 ARCH=i386
+else
+echo "Building Apple x86 boot.efi"
+make ARCH=i386
+fi
+
+cp src/boot/boot.efi boot.32.efi
+
+echo "Building efilipo"
+cd efilipo && make && cd ..
+
+echo "Building universal EFI binary"
+./efilipo/efilipo boot.efi boot.64.efi boot.32.efi
+make clean
+
+rm -f boot.64.efi boot.32.efi
+
+mkdir -p bin
+mv -f boot.efi bin/boot.efi
+
+echo "Build done :-D"
